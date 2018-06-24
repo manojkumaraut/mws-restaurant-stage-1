464d5328ecd10a8a41da4ad544a5f5d4ac30edda
manojkumaraut <manojkumarj.be@gmail.com>
2018-06-24 14:30:22 +0530

    style:added media queries for different devices , responsive formats, and theme color as blue
      - modified the link to button for view detail
      - changed theme color to blue
      - added box shadow for the restaurant list, details and reviews

--- js/main.js
+++ js/main.js
@@ -175,10 +175,11 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button');
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.addEventListener('click', () => { window.location.href = DBHelper.urlForRestaurant(restaurant); });
+  li.append(more);
+  return li
 
   return li
 }
